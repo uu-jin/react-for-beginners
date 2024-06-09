1aba27072e6e9f457cb220c37aaa14ce6b57fdd3
uu-jin <yujinim0321@gmail.com>
2024-06-09 21:43:44 +0900

    css 추가

--- src/router/Home.js
+++ src/router/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
     const [loading, setLoading] = useState(true);
@@ -19,22 +20,27 @@ function Home() {
     }, []);
 
     return (
-        <div>
-          {loading ? <h1>loading...</h1> : (
-            <div>
-                {movies.map((movie) => (
-                  <Movie
-                    key={movie.id}
-                    id={movie.id}
-                    coverImg={movie.medium_cover_image} 
-                    title={movie.title}
-                    summary={movie.summary}
-                    genres={movie.genres}
-                  />
-                ))}
-            </div>
-          )}
-        </div>
+      <div className={styles.container}>
+        {loading ? (
+          <div className={styles.loader}>
+            <span>Loading...</span>
+          </div>
+        ) : (
+          <div className={styles.movies}>
+              {movies.map((movie) => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  coverImg={movie.medium_cover_image} 
+                  title={movie.title}
+                  summary={movie.summary}
+                  genres={movie.genres}
+                />
+              ))}
+          </div>
+        )}
+      </div>
     )
 }
 
